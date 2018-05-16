252ed2718c9fe79f0adfade66a2e4c807f270468
tbielaszewski <tomekbielaszewski@gmail.com>
2018-05-16 22:16:55 +0200

    Example 2_1 removing code

--- src/main/java/pl/grizwold/java8_lambda_deep_dive/Example2_1_MethodReference.java
+++ src/main/java/pl/grizwold/java8_lambda_deep_dive/Example2_1_MethodReference.java
@@ -1,33 +1,11 @@
 package pl.grizwold.java8_lambda_deep_dive;
 
-import java.util.Objects;
-import java.util.function.*;
-
 public class Example2_1_MethodReference {
     public static void main(String[] args) {
         new Example2_1_MethodReference().run();
     }
 
-    private String someString = "Lubie placki";
-
-    private void run() {
-        //Objects::nonNull;
-
-        //this::getSomeString;
-        //Example2_1_MethodReference::getSomeString;
-
-
-        //this::setSomeString;
-        //Example2_1_MethodReference::setSomeString;
-
-
-        //this::printStrings;
-        //Example2_1_MethodReference::printStrings;
-
-
-        //this::concat;
-        //Example2_1_MethodReference::concat;
-    }
+    private String someString = "my sweet string";
 
     private String getSomeString() {
         return someString;
@@ -45,13 +23,17 @@ public class Example2_1_MethodReference {
         System.out.println(concat(someString1, someString2));
     }
 
-//    @FunctionalInterface
-//    interface TriConsumer<IN1, IN2, IN3> {
-//        void accept(IN1 attribute1, IN2 attribute2, IN3 attribute3);
-//    }
-//
-//    @FunctionalInterface
-//    interface TriFunction<CLASS, OUT, IN1, IN2> {
-//        OUT apply(CLASS clazz, IN1 attribute1, IN2 attribute2);
-//    }
+    private void run() {
+//        this::getSomeString;
+//        Example2_1_MethodReference::getSomeString;
+
+//        this::setSomeString;
+//        Example2_1_MethodReference::setSomeString;
+
+//        this::printStrings;
+//        Example2_1_MethodReference::printStrings;
+
+//        this::concat;
+//        Example2_1_MethodReference::concat;
+    }
 }
